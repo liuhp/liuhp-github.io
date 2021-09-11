7d74fb8e53f291b1cf28f993e66cbefc059d46df
liuhp <2190098961@qq.com>
2021-09-11 23:50:27 +0800

    feat: 全局音乐功能呢

--- scripts/plugins.js
+++ scripts/plugins.js
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-09-11 16:44:49
- * @LastEditTime: 2021-09-11 17:04:58
+ * @LastEditTime: 2021-09-11 23:33:59
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /liuhp.github.io/scripts/plugins.js
  */
 hexo.extend.filter.register('theme_inject', function (injects) {
-  // hexo.theme.config.daovoice 就是上面配置的值，我们将配置参数传递给 daovoice.swig
-  // injects.head.file('daovoice', 'source/_data/DaoVoice.swig', {daovoice: hexo.theme.config.daovoice});
+  // 引入 APlayer
+  injects.head.file('aplayer', 'source/_data/APlayer.swig', {aplayer: hexo.theme.config.aplayer});
 });
